9fa1884cc3419e2506d7f8ef57d8657f88ca2bf6
hustr <wangyaning6166@gmail.com>
2018-11-27 22:59:26 +0800

    RDT GBN finished

---
+++ RDT/src/StopWait.cpp
@@ -0,0 +1,30 @@
+
+
+#include "../include/Base.h"
+#include "../include/Global.h"
+#include "../include/RdtSender.h"
+#include "../include/RdtReceiver.h"
+#include "../include/StopWaitRdtSender.h"
+#include "../include/StopWaitRdtReceiver.h"
+#include "GBNSender.h"
+#include "GBNReceiver.h"
+
+
+int main(int argc, char *argv[]) {
+    RdtSender *ps = new GBNSender();
+    RdtReceiver *pr = new GBNReceiver();
+    pns->init();
+    pns->setRtdSender(ps);
+    pns->setRtdReceiver(pr);
+    pns->setInputFile("/home/yaning/Documents/clion/stop_wait/src/input.txt");
+    pns->setOutputFile("/home/yaning/Documents/clion/stop_wait/src/output.txt");
+    pns->start();
+
+    delete ps;
+    delete pr;
+    delete pUtils;                                    //指向唯一的工具类实例，只在main函数结束前delete
+    delete pns;                                        //指向唯一的模拟网络环境类实例，只在main函数结束前delete
+
+    return 0;
+}
+
---
+++ RDT/src/config.h
@@ -0,0 +1,21 @@
+//
+// Created by yaning on 18-11-27.
+//
+
+#ifndef STOP_WAIT_CONFIG_H
+#define STOP_WAIT_CONFIG_H
+
+#include "Global.h"
+#include <cstring>
+#include <utility>
+#include <algorithm>
+#include <mutex>
+#include <cassert>
+
+
+#define BITS 3
+#define WINDOW_WIDTH 4
+
+//constexpr static const int MOD = std::min(1 << BITS, WINDOW_WIDTH - 1);
+
+#endif //STOP_WAIT_CONFIG_H
